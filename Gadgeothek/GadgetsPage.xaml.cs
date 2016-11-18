039c19e18f936eda86e558b17caead0441d60e31
Curlie82 <sebnem.basar@gmail.com>
2016-11-18 21:05:04 +0100

    First Upload to Github

---
+++ Gadgeothek/GadgetsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace Gadgeothek
+{
+    /// <summary>
+    /// Interaktionslogik für GadgetsPage.xaml
+    /// </summary>
+    public partial class GadgetsPage : Page
+    {
+        public GadgetsPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public void ButtonAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        public void ButtonEdit_OnClick(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        public void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+    }
+
+}
---
+++ Gadgeothek/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Gadgeothek
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Page page = new GadgetsPage();
+            GadgetFrame.Navigate(page);
+        }
+
+        public MainWindow(TabItem tabItem)
+        {
+            InitializeComponent();
+
+            if (tabItem.Name == "GadgetItem")
+            {
+                //IsSelected has to be set manually, else the switching of the tabs won't work properly
+                GadgetItem.IsSelected = true;
+                LoanItem.IsSelected = false;
+
+            }
+            else if (tabItem.Name == "LoanItem")
+            {
+                GadgetItem.IsSelected = false;
+                LoanItem.IsSelected = true;
+            }
+        }
+    }
+}
+
